f2b8907fc0b9dcda4b9b1e1617707a27ec206aa9
rtk19626-sketch <rtk19626@email.vccs.edu>
2026-03-12 04:04:01 +0000

    Project 2

--- main.cpp
+++ main.cpp
@@ -1,3 +1,13 @@
 #include <iostream>
-
-int main() { std::cout << "Hello Ryan!\n"; }
+int main()  {
+const double RISE_RATE = 1.5;
+int years1= 5;
+int years2= 7;
+int years3= 10;
+double rise1= years1 * RISE_RATE;
+double rise2= years2 * RISE_RATE;
+double rise3= years3 * RISE_RATE;
+std::cout << "In " << years1 << " years, the ocean will have risen " << rise1 << " millimeters." << std::endl;
+std::cout << "In " << years2 << " years, the ocean will have risen " << rise2 << " millimeters." << std::endl;
+std::cout << "In " << years3 << " years, the ocean will have risen " << rise3 << " millimeters." << std::endl;
+}
